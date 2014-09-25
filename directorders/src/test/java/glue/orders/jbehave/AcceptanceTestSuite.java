b725c3dd50f70ec877a5c49a6cb18fdb7af6c188
Srinivasa.Kuncha <Srinivasa.Kuncha@CIT-DIL-PRE0035.anl.dmgt.net>
2014-09-25 15:16:03 +0100

    drivers

--- directorders/src/test/java/glue/orders/jbehave/AcceptanceTestSuite.java
+++ directorders/src/test/java/glue/orders/jbehave/AcceptanceTestSuite.java
@@ -1,5 +1,14 @@
 package glue.orders.jbehave;
 
+import glue.orders.utils.WebDriverConfigurer;
 import net.thucydides.jbehave.ThucydidesJUnitStories;
 
-public class AcceptanceTestSuite extends ThucydidesJUnitStories {}
+public class AcceptanceTestSuite extends ThucydidesJUnitStories {
+	
+	public AcceptanceTestSuite() {
+		
+		WebDriverConfigurer.configureDrivers();
+		
+		}
+	
+}
